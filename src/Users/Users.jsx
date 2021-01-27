38255c025a6c943aa5d485d0c60bdb4ef3b6d724
malanka96 <voksa.kolya@gmail.com>
2021-01-28 01:18:48 +0300

    setUsers

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -2,6 +2,46 @@ import { React } from "react";
 import style from "./Users.module.css";
 
 let Users = (props) => {
+  if (props.users.length === 0) {
+    props.setUsers([
+      {
+        id: 1,
+        avaUrl: "https://klike.net/uploads/posts/2019-03/1551511784_4.jpg",
+        followed: true,
+        fullName: "Nik",
+        status: "im",
+        location: { sity: "Minsk", country: "Belarus" },
+      },
+      {
+        id: 2,
+        avaUrl:
+          "https://meragor.com/files/styles//ava_800_800_wm/avatar-v-ochkah-025.jpg",
+        followed: true,
+        fullName: "Sony",
+        status: "im 2",
+        location: { sity: "Brest", country: "Belarus" },
+      },
+      {
+        id: 3,
+        avaUrl:
+          "https://whatsism.com/uploads/posts/2018-07/1530546770_rmk_vdjbx10.jpg",
+        followed: false,
+        fullName: "Ant",
+        status: "im 3",
+        location: { sity: "Baranovichi", country: "Belarus" },
+      },
+      {
+        id: 4,
+        avaUrl:
+          "https://proprikol.ru/wp-content/uploads/2020/02/kartinki-na-avatarku-dlya-parnej-i-muzhchin-1-1.jpg",
+        followed: true,
+        fullName: "Pasha",
+        status: "im 23",
+        location: { sity: "Kiev", country: "Ukreine" },
+      },
+    ]);
+  }
+
   return (
     <div>
       {props.users.map((u) => (
@@ -11,7 +51,23 @@ let Users = (props) => {
               <img src={u.avaUrl} alt="a" className={style.avaUrl} />
             </div>
             <div>
-              <button>follow</button>
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}
+                >
+                  unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}
+                >
+                  follow
+                </button>
+              )}
             </div>
           </span>
           <span>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -2,51 +2,38 @@ const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 
-
 let initialState = {
-  users: [
-    {id: 1, 
-      avaUrl: 'https://klike.net/uploads/posts/2019-03/1551511784_4.jpg',
-      followed: true, fullName: 'Nik', status: 'im' , location: {sity: 'Minsk', country: 'Belarus'} },
-    {id: 2,
-      avaUrl: 'https://meragor.com/files/styles//ava_800_800_wm/avatar-v-ochkah-025.jpg',
-      followed: true, fullName: 'Sony', status: 'im 2' , location: {sity: 'Brest', country: 'Belarus'} },
-    {id: 3, 
-      avaUrl: 'https://whatsism.com/uploads/posts/2018-07/1530546770_rmk_vdjbx10.jpg',
-      followed: false, fullName: 'Ant', status: 'im 3' , location: {sity: 'Baranovichi', country: 'Belarus'} },
-    {id: 4,
-      avaUrl: 'https://proprikol.ru/wp-content/uploads/2020/02/kartinki-na-avatarku-dlya-parnej-i-muzhchin-1-1.jpg',
-      followed: true, fullName: 'Pasha', status: 'im 23' , location: {sity: 'Kiev', country: 'Ukreine'} },
-  ]
+  users: [ ]
 };
 
 const usersPageReducer = (state = initialState, action) => {
   switch (action.type) {
     case FOLLOW:
-     return {
-       ...state,
-        users: state.users.map(u => {
-          if (u.id === action.usersId) {
-               return {...u, followed: true}
+      return {
+        ...state,
+        users: state.users.map((u) => {
+          if (u.id === action.userId) {
+            return { ...u, followed: true };
           }
           return u;
-        })
-     }
+        }),
+      };
     case UNFOLLOW:
       return {
         ...state,
-         users: state.users.map(u => {
-           if (u.id === action.usersId) {
-                return {...u, followed: false}
-           }
-           return u;
-         })
-      }
-    case SET_USERS: 
-    return {
-      ...state, users: [...state.users, ...action.users]
-    }
-      
+        users: state.users.map((u) => {
+          if (u.id === action.userId) {
+            return { ...u, followed: false };
+          }
+          return u;
+        }),
+      };
+    case SET_USERS:
+      return {
+        ...state,
+        users: [...state.users, ...action.users],
+      };
+
     default:
       return state;
   }

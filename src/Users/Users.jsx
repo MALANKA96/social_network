ba5f135276d8d3ccb9c89971fc99ab7ac510d1a1
malanka96 <voksa.kolya@gmail.com>
2021-02-04 07:18:07 +0300

    user_API

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -1,10 +1,20 @@
 import { React } from "react";
 import style from "./Users.module.css";
+import * as axios from 'axios';
+import userPhoto from "../assets/image/user.png";
 
 let Users = (props) => {
-   if (props.users.length === 0) { 
-    props.setUsers( [
-        {
+  if (props.users.length === 0) {
+    
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        
+        props.setUsers(response.data.items);
+      });
+
+    /*  props.setUsers([
+      {
         id: 1,
         avaUrl: "https://klike.net/uploads/posts/2019-03/1551511784_4.jpg",
         followed: true,
@@ -38,39 +48,54 @@ let Users = (props) => {
         fullName: "Pasha",
         status: "im 23",
         location: { sity: "Kiev", country: "Ukreine" },
-      } 
-    ])
-   }
+      },
+    ]); */
+  }
 
-  return <div>
-      {
-      props.users.map(u => (
+  return (
+    <div>
+      {props.users.map((u) => (
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.avaUrl} alt="a" className={style.avaUrl} />
+              <img src={u.photos.small != null ? u.photos.small : userPhoto } alt="a" className={style.avaUrl} />
             </div>
-            <div> 
-              {u.followed 
-            ? ( <button onClick={() => { props.unfollow(u.id) }}> unfollow </button>) 
-            : ( <button onClick={() => { props.follow(u.id) }} > follow </button> )}
+            <div>
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}
+                >
+                  {" "}
+                  unfollow{" "}
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}
+                >
+                  {" "}
+                  follow{" "}
+                </button>
+              )}
             </div>
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.sity}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.sity"}</div>
             </span>
           </span>
         </div>
-      ))
-      }
+      ))}
     </div>
-  
+  );
 };
 
 export default Users;
